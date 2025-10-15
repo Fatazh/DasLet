31d23d6243ccedde838fa8ef06aefede16f5a7b5
LAPTOP-C89085J0\2080 <majidilkhithar@hotmail.com>
2025-10-15 17:02:35 +0700

    Fix: perbaikan Layout

--- js/main.js
+++ js/main.js
@@ -1,52 +1,54 @@
 const sidebar = document.querySelector(".sidebar");
+const SubMenus = document.querySelectorAll(".sub-menu");
+const buttons = document.querySelectorAll(".sidebar ul button");
 
 function toggleSidebar() {
+  SubMenus.forEach((menu) => {
+    menu.style.height = "0px";
+  });
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+  sidebar.classList.toggle("collapsed");
+}
+
+const onClick = (item) => {
+  document.querySelectorAll(".sub-menu").forEach((menu) => {
+    if (item.nextElementSibling !== menu) {
+      menu.style.height = "0px";
+    }
+  });
+
+  buttons.forEach((btn) => {
+    if (btn !== item) {
+      btn.classList.remove("active");
+    }
+  });
+
+  if (!item.nextElementSibling) {
+    item.classList.toggle("active");
+    return;
+  }
+
+  const subMenu = item.nextElementSibling;
+  const ul = subMenu.querySelector("ul");
+
+  if (!subMenu.clientHeight) {
+    subMenu.style.height = `${ul.clientHeight}px`;
+    item.classList.add("active");
+  } else {
+    subMenu.style.height = "0px";
+    item.classList.remove("active");
+  }
+};
+
+sidebar.addEventListener("mouseleave", () => {
+  if (sidebar.classList.contains("collapsed")) {
     SubMenus.forEach((menu) => {
       menu.style.height = "0px";
     });
-
     buttons.forEach((button) => {
       button.classList.remove("active");
     });
-
-    sidebar.classList.toggle("collapsed");
-}
-
-const SubMenus =
-    document.querySelectorAll(".sub-menu"),
-    buttons=
-    document.querySelectorAll(".sidebar ul button");
-
-const onClick= (item)=>{
-        SubMenus.forEach((menu) => (menu.style.height ="0px"));
-
-        buttons.forEach((btn) => (btn.classList.remove('active')));
-       
-        if(!item.nextElementSibling){
-            item.classList.add("active");
-            return;
-        }
-
-        const subMenu = item.nextElementSibling,
-            ul = subMenu.querySelector('ul');
-
-        if(!subMenu.clientHeight){
-            subMenu.style.height = `${ul.clientHeight}px`;
-            item.classList.add("active");
-        }else{
-            subMenu.style.height = "0px";
-            item.classList.remove("active");
-        }
-    };
-
-sidebar.addEventListener("mouseleave", () => {
-    if (sidebar.classList.contains("collapsed")){
-        SubMenus.forEach((menu) => {
-            menu.style.height = "0px";
-        });
-
-        buttons.forEach((button) => {
-            button.classList.remove("active");
-        });
-    }
+  }
 });
